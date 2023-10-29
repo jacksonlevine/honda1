c671d9b9fce75d9cea61d056d1f03cb2a17ce883
Jackson Levine <jack@netbug.us>
2023-10-29 00:59:15 -0700

    Create makefile.sh

---
+++ makefile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir build
+
+cd build
+
+cmake -DCMAKE_TOOLCHAIN_FILE=~/Documents/vcpkg/scripts/buildsystems/vcpkg.cmake ..
+
+cmake --build . --config Release || {
+    echo "Build failed."
+    exit 1
+}
+
+cp -f Release/main.exe ..
+cp -f Release/glew32.dll ..
+cp -f Release/glfw3.dll ..
+
+echo "Build and file copying completed successfully."
